a229487d519abc38a92c871e1250652efb69f46b
Andre <be0rn99@icloud.com>
2023-01-13 07:24:38 +0500

    WebKit, APIvk, a little updates

--- StudyStoryBoardApp/StudyStoryBoardApp/Controllers/AuthController/AuthViewController.swift
+++ StudyStoryBoardApp/StudyStoryBoardApp/Controllers/AuthController/AuthViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Alamofire
 
 class AuthViewController: UIViewController, UITextFieldDelegate {
     
@@ -21,25 +22,33 @@ class AuthViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var scrollView: UIScrollView!
     
     let startApp = Notification.Name("StartApp")
+    let baseUrl = "https://api.vk.com/method"
     
     //MARK: - Циклы жзини ViewController
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        UIView.animate(withDuration: 1.5, delay: 0) { [weak self] in
+        UIView.animate(withDuration: 2, delay: 0, options: .curveEaseIn) { [weak self] in
             guard let self = self else { return }
             self.backroundImage.alpha = 0.5
             self.secondImageBackround.alpha = 1
         }
+    }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
         NotificationCenter.default.addObserver(self, selector: #selector(animate), name: startApp, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(goToApp), name: Notification.Name("succes"), object: nil)
+        NotificationCenter.default.post(Notification(name: startApp))
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
     }
     
     override func viewDidDisappear(_ animated: Bool) {
         super.viewDidDisappear(animated)
-        
-        NotificationCenter.default.removeObserver(self, name: startApp, object: nil)
+        NotificationCenter.default.removeObserver(self)
         
         buttonLogIn.layer.cornerRadius = 20
         buttonLogIn.titleLabel?.alpha = 1
@@ -50,28 +59,80 @@ class AuthViewController: UIViewController, UITextFieldDelegate {
         buttonLogIn.layoutIfNeeded()
     }
     
+    // Анимация backroundImage
     @objc func animate() {
         print("subscribe succes")
         backroundImage.rotate(duration: 75, route: .left)
         secondImageBackround.rotate(duration: 120, route: .left)
     }
     
-
-    //MARK: - Проверка логина и пароля + unwind segue
-    
-    @IBAction func unwindToAuth(_ unwindSegue: UIStoryboardSegue) {
-    
+    @objc func goToApp() {
+        performSegue(withIdentifier: "login", sender: nil)
     }
     
-    //MARK: - Анимации загрузки и ошибок
+    func getRequest(method: String, parammeters: Parameters) {
+        AF.request(baseUrl + "/" + method, method: .get , parameters: parammeters).responseJSON { json in
+            print(json)
+        }
+    }
+    
+    //MARK: - Анимации загрузки
+    
+    @IBAction func unwindToAuth(_ unwindSegue: UIStoryboardSegue) {
+        guard let identifier = unwindSegue.identifier else { return }
+        if identifier == "succesAuth" {
+            
+            animationLoading()
+            
+            let paramsFriend: Parameters = [
+                "access_token" : Session.share.token,
+                "user_id" : Session.share.userId,
+                "order" : "random",
+                "count" : "8",
+                "fields" : "nickname",
+                "v" : "5.131"
+            ]
+            
+            let paramsGroup: Parameters = [
+                "access_token" : Session.share.token,
+                "user_id" : Session.share.userId,
+                "extended" : "1",
+                "count" : "8",
+                "v" : "5.131"
+            ]
+            
+            let paramsPhoto: Parameters = [
+                "access_token" : Session.share.token,
+                "owner_id" : Session.share.userId,
+                "album_id" : "profile",
+                "count" : "8",
+                "rev" : "0",
+                "v" : "5.131"
+            ]
+            
+            let paramsSearch: Parameters = [
+                "access_token" : Session.share.token,
+                "user_id" : Session.share.userId,
+                "q" : "apple",
+                "type" : "page",
+                "count" : "8",
+                "v" : "5.131"
+            ]
+            
+            getRequest(method: "friends.get", parammeters: paramsFriend)
+            getRequest(method: "groups.search", parammeters: paramsSearch)
+            getRequest(method: "groups.get", parammeters: paramsGroup)
+            getRequest(method: "photos.get", parammeters: paramsPhoto)
+        }
+    }
     
     func animationLoading() {
-
+        
         buttonLogIn.isTouched(false)
-
-        UIView.animate(withDuration: 0.2,delay: 0, options: .curveEaseOut) { [weak self] in
+        
+        UIView.animate(withDuration: 0.2, delay: 0.5, options: .curveEaseOut) { [weak self] in
             guard let self = self else { return }
-
+            
             self.buttonLogIn.layer.cornerRadius = 32
             self.constraintRight.constant = 163
             self.buttonLogIn.titleLabel?.alpha = 0
@@ -86,10 +147,9 @@ class AuthViewController: UIViewController, UITextFieldDelegate {
             }
         }
     }
-    
-    
-    
 }
+
+//MARK: - Дополнения к контроллеру
 
 extension UIImageView{
     func rotate(duration: Double, route: RouteRotate) {
--- StudyStoryBoardApp/StudyStoryBoardApp/Controllers/AuthController/RegistrationViewController.swift
+++ StudyStoryBoardApp/StudyStoryBoardApp/Controllers/AuthController/RegistrationViewController.swift
@@ -8,17 +8,89 @@
 import UIKit
 import WebKit
 
-@IBDesignable class RegistrationViewController: UIViewController, UITextFieldDelegate {
+class RegistrationViewController: UIViewController {
     
     @IBOutlet weak var webKitView: WKWebView!
     @IBOutlet weak var updateButton: UIBarButtonItem!
+    @IBOutlet weak var loadingIndicator: UIActivityIndicatorView!
+    @IBOutlet weak var closeButton: UIBarButtonItem!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        updateButton.isHidden = true
+        loadingIndicator.startAnimating()
         
+        webKitView.navigationDelegate = self
+        authUrl()
     }
     
     @IBAction func updatePage(_ sender: Any) {
+        webKitView.reloadFromOrigin()
+        authUrl()
+    }
+    
+    func authUrl() {
+        var url = URLComponents()
+        url.scheme = "https"
+        url.host = "oauth.vk.com"
+        url.path = "/authorize"
+        url.queryItems = [
+            URLQueryItem(name: "client_id", value: "8025529"),
+            URLQueryItem(name: "redirect_url", value: "https://oauth.vk.com/blank.html"),
+            URLQueryItem(name: "display", value: "mobile"),
+            URLQueryItem(name: "response_type", value: "token"),
+            URLQueryItem(name: "revoke", value: "1"),
+            URLQueryItem(name: "scope", value: "273887")
+        ]
+        
+        let req = URLRequest(url: url.url!)
+        webKitView.load(req)
     }
 }
 
+extension RegistrationViewController: WKNavigationDelegate {
+    func webView(_ webView: WKWebView, decidePolicyFor navigationResponse: WKNavigationResponse, decisionHandler: @escaping (WKNavigationResponsePolicy) -> Void)  {
+        
+        guard let url = navigationResponse.response.url,
+              url.path() == "/blank.html",
+              let fragment = url.fragment
+        else {
+            decisionHandler(.allow)
+            return
+        }
+        
+        let params = fragment.components(separatedBy: "&")
+            .map { $0.components(separatedBy: "=") }
+            .reduce([String: String]()) { partialResult, param in
+                
+                var dict = partialResult
+                let key = param[0]
+                let value = param[1]
+                dict[key] = value
+                return dict
+            }
+        Session.share.token = params["access_token"]!
+        Session.share.userId = params["user_id"]!
+        
+        decisionHandler(.allow)
+        
+        if Session.share.token != "" {
+            performSegue(withIdentifier: "succesAuth", sender: nil)
+        }
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 3.5) {
+            let notifyName = Notification.Name("succes")
+            NotificationCenter.default.post(Notification(name: notifyName))
+        }
+    }
+    
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        updateButton.isHidden = false
+        loadingIndicator.isHidden = true
+    }
+    
+    func webView(_ webView: WKWebView, didStartProvisionalNavigation navigation: WKNavigation!) {
+        updateButton.isHidden = true
+        loadingIndicator.isHidden = false
+    }
+}
